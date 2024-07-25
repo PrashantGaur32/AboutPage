aadc0d4a9dd7f3c7941eb5d70ba25f37e7d5c0bb
Prashant Gaur <128351288+PrashantGaur32@users.noreply.github.com>
2024-07-25 12:58:55 +0530

    Add files via upload

---
+++ About.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Jalwayu_Project
+{
+    /// <summary>
+    /// Interaction logic for About.xaml
+    /// </summary>
+    public partial class About : Window
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+
+        private void login_Click(object sender, RoutedEventArgs e)
+        {
+            jalwayu jalwayuwindow = new jalwayu();
+            jalwayuwindow.Show();
+        }
+    }
+}
